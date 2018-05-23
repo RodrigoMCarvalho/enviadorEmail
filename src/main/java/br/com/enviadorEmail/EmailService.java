1fecf6ba107e6f4282c4070d0c2950e3c03a4656
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-05-23 18:46:20 -0300

    Projeto destinado ao envio de email

---
+++ src/main/java/br/com/enviadorEmail/EmailService.java
@@ -0,0 +1,31 @@
+package br.com.enviadorEmail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	public void enviar(String nome, String emailDestinatario) {
+		
+		try {
+			Email email = new SimpleEmail();
+			email.setHostName("smtp.googlemail.com");
+			email.setSmtpPort(465);
+			email.setAuthentication("javarodrigo9@gmail.com", "senha");
+			email.setSSLOnConnect(true);
+			
+			email.setFrom("javarodrigo9@gmail.com");
+			email.setSubject("Parabéns, você foi convidado para a ListaVIP");
+			email.setMsg("Olá " + nome +".Você acaba de ser convidado para a ListaVIP");
+			email.addTo(emailDestinatario);
+			email.send();
+			
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
